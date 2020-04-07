4d2a378d10a4f480dd0a0cea5b9c02c9e128ea2b
Michael Lin <hi@michaellin.me>
2020-04-06 22:23:36 -0600

    update test cases

--- tests/open.js
+++ tests/open.js
@@ -2,45 +2,52 @@ const test = require('ava');
 const countBy = require('lodash.countby');
 const puppeteer = require('puppeteer-core');
 const {execSync} = require('child_process');
-const open = require('../dist'); // Run yarn build prior running the test
+const open = require('../src');
 
+const browserName = 'Google Chrome';
 const openUrl = 'http://127.0.0.1:8000/';
-
-function getOpenTabs(browserName) {
-  if (process.platform !== 'darwin') throw new Error('Only support macOS.');
-  const openTabs = execSync(`bash tests/getOpenTabs.sh "${browserName}"`)
-    .toString('utf-8')
-    .split('\n');
-  return openTabs;
+let chromeExecutablePath;
+if (process.platform === 'darwin') {
+  chromeExecutablePath = `/Applications/${browserName}.app/Contents/MacOS/${browserName}`;
+} else if (process.platform === 'linux') {
+  chromeExecutablePath = 'google-chrome';
+} else if (process.platform === 'win32') {
+  chromeExecutablePath = 'Chrome';
 }
 
-function closeOpenTbas(browserName) {
-  if (process.platform !== 'darwin') throw new Error('Only support macOS.');
-  execSync(`bash tests/closeOpenTabs.sh "${browserName}"`, {windowsHide: true});
+console.info(`
+OS: ${process.platform}
+Chrome Path: ${chromeExecutablePath}
+`);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-test('the same tab is reused in browser', async t => {
-  const browserName = 'Google Chrome';
+test.serial('the same tab is reused in browser on macOS', async t => {
   if (process.platform === 'darwin') {
-    // Close all open tabs
-    // closeOpenTbas(browserName);
     const browser = await puppeteer.launch({
       headless: false,
-      executablePath:
-        `/Applications/${browserName}.app/Contents/MacOS/${browserName}`,
+      executablePath: chromeExecutablePath,
     });
+    const page = await browser.newPage();
+    await page.goto(openUrl);
 
-    // Open url with open twice
-    await open(openUrl, {app: browserName, wait: true});
-    await open(openUrl, {app: browserName, wait: true});
+    // Open url with open
+    await open(openUrl);
 
-    // Get open tabs
-    const openTabs = getOpenTabs(browserName);
-    const openTabsCounter = countBy(openTabs);
-    t.is(openTabsCounter[openUrl], 1);
+    // Workaround since new pages are not avaliable immediately
+    // https://github.com/puppeteer/puppeteer/issues/1992#issuecomment-444857698
+    await sleep(5000);
 
-    // Close all open tabs
-    // closeOpenTbas(browserName);
+    // Get open pages/tabs
+    const openPages = (await browser.pages()).map(each => each.url());
+    const openPagesCounter = countBy(openPages);
+
+    // Expect only one page is open
+    t.is(openPagesCounter[openUrl], 1);
+
+    // Close browser
     await browser.close();
   } else {
     // Skip for non-macOS environments
@@ -48,7 +55,49 @@ test('the same tab is reused in browser', async t => {
   }
 });
 
-// Test('open url in browser', async t => {
-//   await open(openUrl);
-//   t.pass();
-// });
+test.serial('open url in browser', async t => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: chromeExecutablePath,
+  });
+
+  await open(openUrl);
+
+  // Workaround since new pages are not avaliable immediately
+  // https://github.com/puppeteer/puppeteer/issues/1992#issuecomment-444857698
+  await sleep(5000);
+
+  // Get open pages/tabs
+  const openPages = (await browser.pages()).map(each => each.url());
+  const openPagesCounter = countBy(openPages);
+
+  // Expect page is opened
+  t.is(openPagesCounter[openUrl], 1);
+
+  await browser.close();
+});
+
+test.serial(
+  'should not open browser when process.env.BROWSER is none',
+  async t => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      executablePath: chromeExecutablePath,
+    });
+    process.env.BROWSER = 'none';
+
+    // Open url
+    await open(openUrl);
+
+    // Get open pages/tabs
+    const openPages = (await browser.pages()).map(each => each.url());
+    const openPagesCounter = countBy(openPages);
+
+    // Expect no page is opened
+    t.is(openPagesCounter[openUrl], undefined);
+
+    // Clean up
+    process.env.BROWSER = browserName;
+    await browser.close();
+  }
+);
